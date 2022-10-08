4f0d20b5d8d4958f7373357884e898a4ad703584
EndeavoringOrb <aaronbelikoff@gmail.com>
2022-10-08 16:52:02 -0700

    1st commit

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
-# ******************************
-# Make your Code
-# ******************************
+values = list(map(int, input().split(" ")))
+smallest = []
+for i in range(len(values)):
+    for j in range(i,len(values)):
+        if len(values[j]) < len(smallest):
+            smallest = [values[j],i+j]
+    values[i],values[smallest[1]] = values[smallest[1]],values[i]
